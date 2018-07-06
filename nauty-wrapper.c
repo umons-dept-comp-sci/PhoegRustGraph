355243ac80c8f7c45f56db536de983c680084a88
Gauvain Devillez <gauvain.devillez@umons.ac.be>
2018-07-06 13:48:14 +0200

    Added function nauty_wrapper_new
    It uses lists lab and ptn directly instead of a set of fixed vertex to
    allow for more flexibility

--- nauty-wrapper.c
+++ nauty-wrapper.c
@@ -84,4 +84,36 @@ void nauty_wrapper(int n, int m, unsigned char *g, int * or, int *fixed, int nfi
     }
 }
 
+void nauty_wrapper_new(int n, int m, unsigned char *g, int *lab, int *ptn, int *orbits)
+{
+    if (m > 0) {
+        static DEFAULTOPTIONS_GRAPH(options);
+        options.getcanon = 1;
+        options.defaultptn = 0;
+        statsblk stats;
+        graph tab[n * m];
+        init_graph(tab, n, m, g);
+        graph cannon[n * m];
+        densenauty(tab, lab, ptn, orbits, &options, &stats, m, n, cannon);
+    }
+    else {
+        int p = 0;
+        int s = 0;
+        int mi = n;
+        for (int i = 0; i < n; ++i) {
+            if (mi > lab[i]) {
+                mi = lab[i];
+            }
+            if (ptn[i] == 0) {
+                for (int j = s; i <= p; ++j) {
+                    orbits[lab[j]] = mi;
+                }
+                mi = n;
+                s = p+1;
+            }
+            ++p;
+        }
+    }
+}
+
 #endif
