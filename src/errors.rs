dfcc6324d5dcf2714609962248e9dfa4391360d9
Gauvain Devillez <gauvain.devillez@umons.ac.be>
2018-09-07 13:33:43 +0200

    Documented errors module

--- src/errors.rs
+++ src/errors.rs
@@ -1,13 +1,17 @@
+//! Module containing the different specific errors returned by the algorithms.
 use std::error::Error;
 use std::fmt;
 use std::io;
 
+/// Error returned by the functions charged with graph6 format handling when the given format is
+/// incorrect.
 #[derive(Debug)]
 pub struct InvalidGraph6 {
     details: String,
 }
 
 impl InvalidGraph6 {
+    /// Returns a new `InvalidGraph6` error with the given error message.
     pub fn new(msg: &str) -> InvalidGraph6 {
         InvalidGraph6 { details: msg.to_string() }
     }
@@ -31,12 +35,15 @@ impl From<io::Error> for InvalidGraph6 {
     }
 }
 
+/// Error returned by algorithms supporting only connected graphs when they are given a
+/// non-connected graph.
 #[derive(Debug)]
 pub struct DisconnectedGraph {
     details: String,
 }
 
 impl DisconnectedGraph {
+    /// Returns a new `DisconectedGraph` error with given error message.
     pub fn new(msg: &str) -> DisconnectedGraph {
         DisconnectedGraph { details: msg.to_string() }
     }
