a9a88b09e7d7d76fcc3076a2f50ba8bedc2a916d
Gauvain Devillez <gauvain.devillez@umons.ac.be>
2018-10-26 14:49:06 +0200

    Added doc and unit tests to Set

--- src/lib.rs
+++ src/lib.rs
@@ -59,6 +59,11 @@ mod detail {
 }
 
 
+/// Structure to store a set of integers using binary words.
+/// This structure can store all integers from 0 to a maximum value given in parameters to the
+/// constructors. For example, a maximum value of 16 allows storing all integers from 0 to 15.
+/// Since it uses 64 bits binary words, a maximum size of 64 allows using only one word and is thus
+/// faster.
 #[derive(Clone,Debug)]
 pub struct Set {
     data: Vec<set>,
@@ -67,34 +72,78 @@ pub struct Set {
 }
 
 impl Set {
-    fn fill<PF>(maxelem: u64, pfunc: PF, val: set, numelem: u64) -> Set
+    fn initfill<PF>(maxelem: u64, pfunc: PF, val: set, numelem: u64) -> Set
         where PF: Fn(u64) -> set
     {
         unsafe {
-            let mut p = maxelem;
-            let words = detail::setwordsneeded(maxelem as int);
-            let mut v = Vec::with_capacity(words as usize);
-            for _ in 0..words - 1 {
-                v.push(val);
-                p -= detail::wordsize() as u64;
-            }
-            v.push(pfunc(p));
-            Set {
-                data: v,
-                maxm: maxelem,
-                size: numelem,
+            if maxelem > 0 {
+                let mut p = maxelem;
+                let words = detail::setwordsneeded(maxelem as int) as u64;
+                let mut v = Vec::with_capacity(words as usize);
+                for _ in 0..words - 1 {
+                    v.push(val);
+                    p -= detail::wordsize() as u64;
+                }
+                v.push(pfunc(p));
+                Set {
+                    data: v,
+                    maxm: maxelem,
+                    size: numelem,
+                }
+            } else {
+                Set {
+                    data: vec![],
+                    maxm: 0,
+                    size: 0,
+                }
             }
         }
     }
 
-    pub fn new(maxm: u64) -> Set {
-        Set::fill(maxm, |_| 0, 0, 0)
+    /// Returns an empty set that can contain elements up to `max`-1 (from 0 to `max`-1).
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s = Set::new(0);
+    /// assert_eq!(s.size(),0);
+    /// s = Set::new(112);
+    /// assert_eq!(s.size(), 0);
+    /// for i in 0..112 {
+    ///     assert!(!s.contains(i));
+    /// }
+    /// ```
+    pub fn new(max: u64) -> Set {
+        Set::initfill(max, |_| 0, 0, 0)
     }
 
-    pub fn full(maxm: u64) -> Set {
-        unsafe { Set::fill(maxm, |x| detail::allmask(x as int), detail::allbits(), maxm) }
+    /// Returns a set with with all the elements from 0 to `max`-1.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s = Set::full(112);
+    /// assert_eq!(s.size(),112);
+    /// for i in 0..112 {
+    ///     assert!(s.contains(i));
+    /// }
+    /// ```
+    pub fn full(max: u64) -> Set {
+        unsafe { Set::initfill(max, |x| detail::allmask(x as int), detail::allbits(), max) }
     }
 
+    /// Removes all elements from the set.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s = Set::full(112);
+    /// s.clear();
+    /// assert_eq!(s.size(),0);
+    /// for i in 0..112 {
+    ///     assert!(!s.contains(i));
+    /// }
+    /// ```
     pub fn clear(&mut self) {
         unsafe {
             self.size = 0;
@@ -102,21 +151,118 @@ impl Set {
         }
     }
 
+    /// Adds all possible elements to the set.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s = Set::new(112);
+    /// s.add(34);
+    /// s.add(100);
+    /// s.fill();
+    /// assert_eq!(s.size(), 112);
+    /// for i in 0..112 {
+    ///     assert!(s.contains(i));
+    /// }
+    /// ```
+    pub fn fill(&mut self) {
+        if self.maxm > 0 {
+            unsafe {
+                let p = detail::setwordsneeded(self.maxm as int);
+                let mut rem = self.maxm as u64;
+                for i in 0..p - 1 {
+                    self.data[i as usize] = detail::allbits();
+                    rem -= detail::wordsize() as u64;
+                }
+                *self.data.last_mut().unwrap() = detail::allmask(rem as int);
+                self.size = self.maxm;
+            }
+        }
+    }
+
+    /// Adds element `elem` to the set.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s = Set::new(112);
+    /// let elems: Vec<u64> = vec![0,34,67,111];
+    /// for i in &elems {
+    ///     s.add(*i);
+    /// }
+    /// assert_eq!(s.size(),elems.len() as u64);
+    /// for &i in &elems {
+    ///     assert!(s.contains(i));
+    /// }
+    /// assert!(std::panic::catch_unwind(move || s.add(112)).is_err());
+    /// ```
+    ///
+    /// Panics :
+    /// if `elem > set.getmax()`
     pub fn add(&mut self, elem: u64) {
+        assert!(elem < self.maxm);
         if !self.contains(elem) {
             self.size += 1;
             unsafe { detail::addelement(self.data.as_mut_ptr(), elem as int) }
         }
     }
 
+    /// Removes element `elem` from the set.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s = Set::full(112);
+    /// let elems: Vec<u64> = vec![0,34,67,111];
+    /// for i in &elems {
+    ///     s.remove(*i);
+    /// }
+    /// assert_eq!(s.size(),112-elems.len() as u64);
+    /// for &i in &elems {
+    ///     assert!(!s.contains(i));
+    /// }
+    /// assert!(std::panic::catch_unwind(move || s.remove(112)).is_err());
+    /// ```
+    ///
+    /// Panics :
+    /// if `elem > set.getmax()`
     pub fn remove(&mut self, elem: u64) {
+        assert!(elem < self.maxm);
         if self.contains(elem) {
             self.size -= 1;
             unsafe { detail::delelement(self.data.as_mut_ptr(), elem as int) }
         }
     }
 
+    /// Toggles element `elem` from the set.
+    /// i.e., if `elem` was in the set, it is removed, if it was not int the set, it is added.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s = Set::new(112);
+    /// let outside: Vec<u64> = vec![2,6,74,107];
+    /// for i in &outside {
+    ///     s.add(*i);
+    /// }
+    /// let inside: Vec<u64> = vec![0,34,67,81,111];
+    /// for i in inside.iter().chain(outside.iter()) {
+    ///     s.flip(*i);
+    /// }
+    /// assert_eq!(s.size(),inside.len() as u64);
+    /// for i in &outside {
+    ///     assert!(!s.contains(*i));
+    /// }
+    /// for i in &inside {
+    ///     assert!(s.contains(*i));
+    /// }
+    /// assert!(std::panic::catch_unwind(move || s.flip(112)).is_err());
+    /// ```
+    ///
+    /// Panics :
+    /// if `elem > set.getmax()`
     pub fn flip(&mut self, elem: u64) {
+        assert!(elem < self.maxm);
         unsafe {
             if self.contains(elem) {
                 self.size -= 1;
@@ -127,20 +273,150 @@ impl Set {
         }
     }
 
+    /// Returns `true` if the set contains `elem` and `false` otherwise.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut elems = vec![0,4,1,10,9,66,74,7];
+    /// let s: Set = elems.as_slice().into();
+    /// elems.sort();
+    /// assert_eq!(s.size(),8);
+    /// assert_eq!(s.getmax(),75);
+    /// let mut p = 0;
+    /// for i in 0..75 {
+    ///     if i == elems[p] {
+    ///         assert!(s.contains(i));
+    ///         p += 1;
+    ///     } else {
+    ///         assert!(!s.contains(i));
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// Panics :
+    /// if `elem > set.getmax()`
     pub fn contains(&self, elem: u64) -> bool {
+        assert!(elem < self.maxm);
         unsafe { detail::iselement(self.data.as_ptr(), elem as int) }
     }
 
+    /// Returns the number of elements in the set.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s = Set::new(154);
+    /// assert_eq!(s.size(),0);
+    /// s.add(0);
+    /// s.add(10);
+    /// s.add(130);
+    /// assert_eq!(s.size(),3);
+    /// s.fill();
+    /// assert_eq!(s.size(),154);
+    /// s.clear();
+    /// assert_eq!(s.size(),0);
+    /// ```
     pub fn size(&self) -> u64 {
         self.size
     }
 
+    /// Returns an iterator over the elements of the set.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let elements: &[u64] = &[3,9,25,66];
+    /// let mut s: Set = elements.into();
+    /// for (i,e) in s.iter().enumerate() {
+    ///     assert_eq!(e,elements[i]);
+    /// }
+    /// ```
     pub fn iter(&self) -> impl Iterator<Item = u64> + '_ {
         SetIter::new(self.data.as_ptr(), self.data.len() as u64)
     }
 
-    pub fn maxm(&self) -> u64 {
+    /// Returns the maximum number of elements that the set can contain.
+    /// Note that this is also the maximum element that can be added.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s = Set::new(0);
+    /// assert_eq!(s.getmax(),0);
+    /// s = Set::new(23);
+    /// assert_eq!(s.getmax(),23);
+    /// ```
+    pub fn getmax(&self) -> u64 {
         self.maxm
+    }
+
+    /// Changes the maximum value that can be stored in the set and the maximum size of the set.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut set = Set::full(112);
+    /// set.remove(7);
+    /// set.setmax(20);
+    /// assert_eq!(set.size(),19);
+    /// assert_eq!(set.getmax(),20);
+    /// for i in 0..20 {
+    ///     if i != 7 {
+    ///         assert!(set.contains(i));
+    ///     } else {
+    ///         assert!(!set.contains(i));
+    ///     }
+    /// }
+    /// set.setmax(120);
+    /// assert_eq!(set.size(),19);
+    /// assert_eq!(set.getmax(),120);
+    /// for i in 0..120 {
+    ///     if i < 20 && i != 7 {
+    ///         assert!(set.contains(i));
+    ///     } else {
+    ///         assert!(!set.contains(i));
+    ///     }
+    /// }
+    /// set.fill();
+    /// assert_eq!(set.size(),120);
+    /// assert_eq!(set.getmax(),120);
+    /// for i in 0..120 {
+    ///     assert!(set.contains(i));
+    /// }
+    /// ```
+    pub fn setmax(&mut self, nmax: u64) {
+        unsafe {
+            if self.maxm != nmax {
+                let words = self.data.len() as u64;
+                let nw = detail::setwordsneeded(nmax as int) as u64;
+                if nw == 0 {
+                    self.data.clear();
+                    self.maxm = nmax;
+                    self.size = 0;
+                } else if nmax < self.maxm {
+                    if nw < words {
+                        let diff = words - nw;
+                        for _ in 0..diff {
+                            self.data.pop();
+                            self.maxm -= detail::wordsize() as u64;
+                        }
+                    }
+                    let rem = nmax - (nw - 1) * detail::wordsize() as u64;
+                    {
+                        let lst = self.data.last_mut().unwrap();
+                        *lst &= detail::allmask(rem as int) as set;
+                    }
+                    self.size = detail::setsize(self.data.as_ptr(), nw as int) as u64;
+                } else if nw > words {
+                    let diff = nw - words;
+                    for _ in 0..diff {
+                        self.data.push(0);
+                    }
+                }
+                self.maxm = nmax;
+            }
+        }
     }
 
     fn combine<C>(first: &mut Set, other: &Set, comb: C)
@@ -151,45 +427,145 @@ impl Set {
         }
     }
 
+    /// Removes the elements in the set that are not present in the other set.
+    /// If you do not want your set to be modified, make a copy first.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s1: Set = vec![3,8,0,123].as_slice().into();
+    /// let mut s2: Set = vec![5,12,0,123].as_slice().into();
+    /// s1.inter(&s2);
+    /// assert_eq!(s1.size(),2);
+    /// for &i in &[0,123] {
+    ///     assert!(s1.contains(i as u64));
+    /// }
+    /// for &i in &[3,8,12] {
+    ///     assert!(!s1.contains(i as u64));
+    /// }
+    /// s1 = vec![3,8,0,123].as_slice().into();
+    /// s2 = vec![].as_slice().into();
+    /// s1.inter(&s2);
+    /// assert_eq!(s1.size(),0);
+    /// for &i in &[0,3,8,123] {
+    ///     assert!(!s1.contains(i as u64));
+    /// }
+    /// ```
     pub fn inter(&mut self, other: &Set) {
         Set::combine(self, other, |a, b| a & b);
         if self.data.len() > other.data.len() {
-            for i in (self.data.len() - other.data.len())..self.data.len() {
+            for i in other.data.len()..self.data.len() {
                 self.data[i] = 0;
             }
         }
+        unsafe {
+            self.size = detail::setsize(self.data.as_ptr(), self.data.len() as int) as u64;
+        }
     }
 
+    /// Adds all the elements from the other set to this set.
+    /// If you do not want your set to be modified, make a copy first.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s1: Set = vec![3,8,0,123].as_slice().into();
+    /// let mut s2: Set = vec![5,12,0,123].as_slice().into();
+    /// s1.union(&s2);
+    /// assert_eq!(s1.size(),6);
+    /// for &i in &[0,3,5,8,12,123] {
+    ///     assert!(s1.contains(i as u64));
+    /// }
+    /// s2 = vec![].as_slice().into();
+    /// s2.union(&s1);
+    /// assert_eq!(s2.size(),6);
+    /// for &i in &[0,3,5,8,12,123] {
+    ///     assert!(s2.contains(i as u64));
+    /// }
+    /// ```
     pub fn union(&mut self, other: &Set) {
         if self.data.len() < other.data.len() {
-            for _ in (other.data.len() - self.data.len())..other.data.len() {
+            for _ in self.data.len()..other.data.len() {
                 self.data.push(0);
             }
         }
-        Set::combine(self, other, |&a, &b| a | b)
-    }
-
-    fn complement_set(s: *mut set, len: u64, maxm: u64) {
-        let mut s = s;
-        let mut maxm = maxm;
+        self.maxm = std::cmp::max(self.maxm, other.maxm);
+        Set::combine(self, other, |&a, &b| a | b);
         unsafe {
-            for _ in 0..(len - 1) {
-                *s = !*s;
-                maxm -= detail::wordsize() as u64;
-                s = s.add(1);
-            }
-            *s = !*s & detail::allmask(maxm as int);
+            self.size = detail::setsize(self.data.as_ptr(), self.data.len() as int) as u64;
         }
     }
 
+    fn complement_set(s: *mut set, len: u64, maxm: u64) {
+        if len > 0 {
+            let mut s = s;
+            let mut maxm = maxm;
+            unsafe {
+                for _ in 0..(len - 1) {
+                    *s = !*s;
+                    maxm -= detail::wordsize() as u64;
+                    s = s.add(1);
+                }
+                *s = !*s & detail::allmask(maxm as int);
+            }
+        }
+    }
+
+    /// Flips every element of the set. i.e., each element between 0 and max-1 that was in the set
+    /// is removed and every other element is added.
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut s = Set::new(0);
+    /// s.complement();
+    /// assert_eq!(s.size(),0);
+    /// s = vec![1,3,6,333].as_slice().into();
+    /// s.complement();
+    /// assert_eq!(s.size(),334-4);
+    /// ```
     pub fn complement(&mut self) {
         Set::complement_set(self.data.as_mut_ptr(), self.data.len() as u64, self.maxm);
+        self.size = self.maxm - self.size;
     }
 }
 
 impl fmt::Display for Set {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self.iter().collect::<Vec<u64>>())
+    }
+}
+
+impl<'a> From<&'a [u64]> for Set {
+    /// Converts a slice to a Set
+    ///
+    /// Examples :
+    /// ```
+    /// use graph::Set;
+    /// let mut elems = vec![0,4,1,10,9,66,74,7];
+    /// let s: Set = elems.as_slice().into();
+    /// elems.sort();
+    /// assert_eq!(s.size(),8);
+    /// assert_eq!(s.getmax(),75);
+    /// let mut p = 0;
+    /// for i in 0..75 {
+    ///     if i == elems[p] {
+    ///         assert!(s.contains(i));
+    ///         p += 1;
+    ///     } else {
+    ///         assert!(!s.contains(i));
+    ///     }
+    /// }
+    /// ```
+    fn from(slice: &[u64]) -> Self {
+        let mut set = Set::new(0);
+        for &i in slice {
+            if i >= set.getmax() {
+                set.setmax(i + 1);
+            }
+            set.add(i.into());
+        }
+        set
     }
 }
 
