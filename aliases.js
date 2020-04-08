8dad8b28568b0205d701462fafac523ea85c82d1
Gauvain Devillez <gauvain.devillez@umons.ac.be>
2020-04-08 15:40:26 +0200

    Update documentation

--- aliases.js
+++ aliases.js
@@ -2,6 +2,7 @@ var ALIASES = {};
 ALIASES["base64"] = {};
 ALIASES["bit_vec"] = {};
 ALIASES["byteorder"] = {};
+ALIASES["doc_comment"] = {};
 ALIASES["graph"] = {};
 ALIASES["lazy_static"] = {};
 ALIASES["libc"] = {};
--- source-files.js
+++ source-files.js
@@ -2,6 +2,7 @@ var N = null;var sourcesIndex = {};
 sourcesIndex["base64"] = {"name":"","dirs":[{"name":"write","files":["encoder.rs","mod.rs"]}],"files":["chunked_encoder.rs","decode.rs","display.rs","encode.rs","lib.rs","tables.rs"]};
 sourcesIndex["bit_vec"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["byteorder"] = {"name":"","files":["io.rs","lib.rs"]};
+sourcesIndex["doc_comment"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["graph"] = {"name":"","files":["algorithm.rs","errors.rs","format.rs","invariants.rs","lib.rs","nauty.rs","subgraphs.rs","transfo_result.rs","transfos.rs","transfos_macros.rs"]};
 sourcesIndex["lazy_static"] = {"name":"","files":["inline_lazy.rs","lib.rs"]};
 sourcesIndex["libc"] = {"name":"","dirs":[{"name":"unix","dirs":[{"name":"linux_like","dirs":[{"name":"linux","dirs":[{"name":"gnu","dirs":[{"name":"b64","dirs":[{"name":"x86_64","files":["align.rs","mod.rs","not_x32.rs"]}],"files":["mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["mod.rs"]}],"files":["align.rs","mod.rs"]}],"files":["fixed_width_ints.rs","lib.rs","macros.rs"]};
