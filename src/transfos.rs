fbf55943dd33db5db8d6af75474b311c11cb8a14
Gauvain Devillez <gauvain.devillez@umons.ac.be>
2021-05-19 17:11:06 +0200

    Remove num edges now adds the number in its text

--- src/transfos.rs
+++ src/transfos.rs
@@ -335,7 +335,7 @@ pub fn remove_num_edges(g: &GraphNauty, e: u64) -> Vec<GraphTransformation>
                     for (x,y) in stack.iter() {
                         ng.remove_edge(*x, *y);
                     }
-                    ng.set_name("remove_num_edges".to_string());
+                    ng.set_name(format!("remove_{}_edges", e));
                     res.push(ng);
                 } else {
                     stack.push(found_edge);
